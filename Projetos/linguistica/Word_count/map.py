3a7b1fe5aa9edf1f61bc2a1951029ed5af7c5a5b
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-11-01 17:05:26 -0200

    Create map.py

---
+++ Projetos/linguistica/Word_count/map.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from sys import stdin
+
+for line in stdin:
+    line = line.strip()
+    words = line.split()
+    for word in words:
+        word = word.lower().replace("!"," !")
+        word = word.replace("?"," ? ").replace(","," , ")
+        word = word.replace(";"," ; ").replace("..."," ... ")
+        word = word.replace(":"," : ").replace("("," ( ")
+        word = word.replace(")"," ) ").replace("."," . ").replace("\""," \" ")
+
+        word = word.split()
+        if len(word) > 1:
+        	for x in word:
+        		print (("%s\t\t%s") % (x, 1))
+
+        else:
+        	print (("%s\t\t%s") % (word[0], 1))
