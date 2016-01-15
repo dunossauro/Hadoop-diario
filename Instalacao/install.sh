64668f0400a3bb5d9d01cb0d5b09c78e8654b558
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2016-01-15 21:39:05 -0200

    Create install.sh
    Ainda não funciona com modo cluster

---
+++ Instalacao/install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+clear
+echo -e "
+##################################################################
+##\t\tBem vindo a instalação do Apache Hadoop 2.7\t##
+##################################################################
+\n\n"
+
+INSTALL_LOCAL=/usr/local/
+HADOOP_ETC=/usr/local/hadoop/etc/hadoop
+
+download(){
+  wget http://mirror.nbtelecom.com.br/apache/hadoop/common/hadoop-2.7.1/hadoop-2.7.1.tar.gz
+  tar xvvf hadoop-*.tar.gz
+  sudo mv hadoop-* $INSTALL_LOCAL
+}
+
+inicio(){
+  echo -e "Desja efetuar do Download? (S/N)"
+  read bool
+
+  case $bool in
+    "s"|"S" ) download ;;
+    "n"|"N") ;;
+    * ) echo -e "Digite uma opção válida"
+        inicio ;;
+  esac
+}
+
+modo_de_instalacao(){
+  echo -e "\n\nDigite o modo de instalação:\n\n\
+  1 - Standalone\n\
+  2 - Pseudo-distributed\n\
+  3 - Cluster-mode\n"
+
+  read mode
+
+  case $mode in
+    1 )
+
+        echo -e "Digite o caminho de instalação do seu java EX: /usr/lib/jvm/default"
+        read CAMINHO_JAVA
+        echo -e "export JAVA_HOME=$CAMINHO_JAVA" >> hadoop-env.sh
+        sudo mv hadoop-env.sh $HADOOP_ETC
+        clear
+        echo "Modo Standalone instalado om sucesso!!!"
+      ;;
+    2 )
+        echo -e "Digite o caminho de instalação do seu java EX: /usr/lib/jvm/default"
+        read CAMINHO_JAVA
+        echo -e "export JAVA_HOME=$CAMINHO_JAVA" >> hadoop-env.sh
+        sudo mv hadoop-env.sh $HADOOP_ETC
+        sudo mv core-site.xml $HADOOP_ETC
+        sudo mv mapred-site.xml $HADOOP_ETC
+        sudo mv yarn-site.xml $HADOOP_ETC
+        sudo mv hdfs-site.xml $HADOOP_ETC
+        clear
+        echo "Modo Pseudo-distributed instalado om sucesso!!!"
+      ;;
+    3 )
+      ;;
+    * ) echo -e "Digite uma opção válida"
+        modo_de_instalacao;;
+  esac
+}
+
+inicio
+modo_de_instalacao
