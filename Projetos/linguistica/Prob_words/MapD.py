2ae78016cd71c427313ff9b49b28f7e170fa3635
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2016-03-29 20:57:06 -0300

    Update MapD.py

--- Projetos/linguistica/Prob_words/MapD.py
+++ Projetos/linguistica/Prob_words/MapD.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
-import sys
+from sys import stdin, argv
+from re import compile
 
-for linha in sys.stdin:
-    linha = linha.strip()
-    palavras = linha.split()
+regex = compile("\w+")
+palavra = argv[1]
 
-    for x,y in enumerate(palavras):
-        if y.lower() == "as":
-            palavras[x+1] = palavras[x+1].lower().replace("!"," !")
-            palavras[x+1] = palavras[x+1].replace("?"," ? ").replace(","," , ")
-            palavras[x+1] = palavras[x+1].replace(";"," ; ").replace("..."," ... ")
-            palavras[x+1] = palavras[x+1].replace(":"," : ").replace("("," ( ")
-            palavras[x+1] = palavras[x+1].replace(")"," ) ").replace("."," . ").replace("\""," \" ")
-            print (("%s\t%s\t1")%(palavras[x].lower(),palavras[x+1].split()[0]))
-        else:
-            pass
+for linha in stdin:
+    linha = regex.findall(linha)
+
+    if palavra in linha:
+        index = linha.index(palavra)
+        try:
+            print("{palavra} {palavra_seg}\t1".format(palavra=linha[index], palavra_seg=linha[index+1]))
+        except:
+            print("{palavra} {palavra_seg}\t1".format(palavra=linha[index], palavra_seg="."))
