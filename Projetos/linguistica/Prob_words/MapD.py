f50381e17cba7c544f643eb8b117be187e481dbd
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2016-03-29 21:14:21 -0300

    Update MapD.py

--- Projetos/linguistica/Prob_words/MapD.py
+++ Projetos/linguistica/Prob_words/MapD.py
@@ -4,10 +4,12 @@ from sys import stdin, argv
 from re import compile
 
 regex = compile("\w+")
-palavra = argv[1]
+palavra = argv[1].lower()
 
 for linha in stdin:
     linha = regex.findall(linha)
+
+    linha = [e.lower() for e in linha]
 
     if palavra in linha:
         index = linha.index(palavra)
