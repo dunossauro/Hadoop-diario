e1eec02fbdf466e804a16f9012cdec209da484ac
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2016-03-29 21:08:58 -0300

    Update ReduceD.py

--- Projetos/linguistica/Prob_words/ReduceD.py
+++ Projetos/linguistica/Prob_words/ReduceD.py
@@ -1,42 +1,21 @@
-#!/usr/bin/env python3
 from sys import stdin
 
-##Print de apresentação e indicação
 print (("Nº |%-20s%s|%-7s|%s") % ("Frase", " " * 8, "Prob", "Cont"))
 print (("%s+%s+%s+%s") % ("-"*3, "-" * 28,"-" * 7 ,"-" * 5))
+dic = {}
+contador = 0
 
-#Variáveis de contagem e organização
-palavra_corrente = None
-contador_corrente = 0
-palavra = None
-contador_total = 0
-dicionario = {}
+for line in stdin:
+    contador += 1
+    line = line.strip()
+    word, count = line.split('\t')
 
-#Iteração para contagem
-for linha in stdin:
-    contador_total += 1
-    linha = linha.strip()
-    palavra_1, palavra_2, contador = linha.split() #Split das palavras vinda do shuffle para contagem
+    count = int(count)
 
-    palavra = (("%s %s") % (palavra_1, palavra_2)) #Junção das palavras, separadas de seu contador
-    contador = int(contador)
-
-    #validação para saber se ainda estamos na mesma palavra
-    if palavra_corrente == palavra:
-        contador_corrente += int(contador)
-
-    #Caso não seja é atribuída a um dicionário a palavra e seu contador
+    if word not in dic:
+        dic[word] =  count
     else:
-        if palavra_corrente:
-            dicionario[palavra_corrente] = contador_corrente
-        contador_corrente = contador
-        palavra_corrente = palavra
+        dic[word] += count
 
-if palavra_corrente == palavra: #Print da ultima iteração
-    dicionario[palavra_corrente] = contador_corrente
-
-#print (('%-20s|%2s') %("Total",contador_total))
-
-#Iteração sobre o dicionário para exibição em ordem alfabética, pode ser substituído por ordem de porcentagem
-for y,x in enumerate(sorted(dicionario)):
-    print(('%s  |\t%-20s\t|%5s%%\t|%5s')%(y,x, int(dicionario[x]/contador_total*100),dicionario[x]))
+for n,x in enumerate(sorted(dic)):
+    print (('{n}  |\t%-20s\t|%0.2f%%\t|%5s') % (n, x, float(dic[x]/contador*100),dic[x]))
